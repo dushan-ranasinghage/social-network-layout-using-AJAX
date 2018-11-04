8eb3161397c45eb56b343f628929ea5caaecbab2
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-04 21:53:48 +0530

    AJAX get method for fetching data

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,1 +1,8 @@
 console.log("TEST DOM")
+$(document).ready(function(){
+    $("button").click(function(){
+        $.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+    });
+});
