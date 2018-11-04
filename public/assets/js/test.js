712daa94b9a521a79b993245cd59f9f670c6f998
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-04 22:24:30 +0530

    Succeufully Rendered Data to the DOM using AJAX

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,14 +1,42 @@
 console.log("CONSOLE WORKS FINE!")
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
+//             // alert("Data: " + data + "\nStatus: " + status);
+//             console.log("STATUS",status);
+//             console.log("DATA",data);
+//             var array = data
+//             for(i=0 ;i<5;i++){
+//                 $("#content").load(array[i].title);
+//             }
+//         });
+//     });
+// });
+
+// FETCH DATA TO THE DOM USING API
+
 $(document).ready(function(){
     $("button").click(function(){
-        $.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
-            // alert("Data: " + data + "\nStatus: " + status);
-            console.log("STATUS",status);
-            console.log("DATA",data);
-            var array = data
-            for(i=0 ;i<5;i++){
-                $("#content").load(array[i].title);
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+      xhttp.send();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var posts = response;
+            console.log(posts);
+            var output = '';
+            for(var i = 0;i < 5;i++){ 
+              // u can use any value for 5 or photos.length
+              output += '<li  class="list-group-item d-flex justify-content-between align-items-center">'+posts[i].title+'</li>';
+              // output += '<li><h5>'+photos[i].title+'</h5></li><br />'+'<img src='
+              // +photos[i].url+
+              // ' alt="Smiley face" height="200" width="200">'+'<button type="button" class="btn btn-danger float-right">Delete</button><br /><br />';
+
             }
-        });
-    });
+            document.getElementById('posts').innerHTML = output;
+          } 
+      };
+
+  });
 });
