f10a20714267d807c5011c1c2996440413985e26
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-04 22:05:53 +0530

    An error orcured while rendering data to the DOM

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,10 +1,14 @@
-console.log("TEST DOM")
+console.log("CONSOLE WORKS FINE!")
 $(document).ready(function(){
     $("button").click(function(){
         $.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
             // alert("Data: " + data + "\nStatus: " + status);
             console.log("STATUS",status);
             console.log("DATA",data);
+            var array = data
+            for(i=0 ;i<5;i++){
+                $("#content").load(array[i].title);
+            }
         });
     });
 });
