70947d080bd595aa750248cdf77e3aadb4763fa9
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-04 21:55:15 +0530

    Getting data to the console

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -2,7 +2,9 @@ console.log("TEST DOM")
 $(document).ready(function(){
     $("button").click(function(){
         $.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
+            // alert("Data: " + data + "\nStatus: " + status);
+            console.log("STATUS",status);
+            console.log("DATA",data);
         });
     });
 });
