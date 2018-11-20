3481102af8315fd2d385d7b23410337a5a511566
Dushan Randika <DushanR@millenniumitesp.com>
2018-11-20 23:21:09 +0530

    Dynamic Search Done so basically

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -44,29 +44,42 @@ $(document).ready(function(){
 });
 
 
+// qui est esse
+
 $(function(){
   $("#search").keypress(function(){
+
+    // Dynamic Search Part Start
       // var search = this.value;
       // console.log("Search Value",search);
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-      xhttp.send();
-      xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            var response = JSON.parse(xhttp.responseText);
-            var posts = response;
+
+
+
+      // var xhttp = new XMLHttpRequest();
+      // xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+      // xhttp.send();
+      // xhttp.onreadystatechange = function() {
+      //     if (this.readyState == 4 && this.status == 200) {
+      //       var response = JSON.parse(xhttp.responseText);
+
+            var posts = [{userId: 1, id: 1, title: "sunt aut facere", body: "quia et suscipit↵suscipit recusandae consequuntur"},
+             {userId: 1, id: 2, title: "qui est esse", body: "est rerum tempore vitae↵sequi sint nihil reprehend…aperiam non "},
+             {userId: 1, id: 3, title: "ea molestias quasi exercitationem ", body: "et iusto sed quo iure↵voluptatem occaecati omnis"},
+             {userId: 1, id: 4, title: "eum et est occaecati", body: "ullam et saepe reiciendis voluptatem adipisci↵sit "},
+             {userId: 1, id: 5, title: "nesciunt quas odio", body: "repudiandae veniam quaerat sunt sed↵alias aut "}];
             console.log(posts);
             var output = '';
             for(var i = 0;i < 5;i++){ 
-                if(posts[i].title=this.value){
+                if(posts[i].title.includes(this.value)){
                   output += '<li  class="list-group-item d-flex justify-content-between align-items-center">'+posts[i].title+'</li>';
                 } else{
                   output += '';
                 }
             }
+            console.log(output);
             document.getElementById('posts').innerHTML = output;
-          } 
-      };
+          // } 
+  //     };
 
   });
 });
