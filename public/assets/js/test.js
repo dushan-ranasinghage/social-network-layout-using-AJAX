ca419b45b82efd6f7be1ed46b08194e5a152f5c1
Dushan Randika <DushanR@millenniumitesp.com>
2018-12-16 22:55:14 +0530

    Added footer section

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,24 +1,9 @@
 console.log("CONSOLE WORKS FINE!")
 
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $.get("https://jsonplaceholder.typicode.com/posts", function(data, status){
-//             // alert("Data: " + data + "\nStatus: " + status);
-//             console.log("STATUS",status);
-//             console.log("DATA",data);
-//             var array = data
-//             for(i=0 ;i<5;i++){
-//                 $("#content").load(array[i].title);
-//             }
-//         });
-//     });
-// });
-
-
-
 // FETCH DATA TO THE DOM USING API
+
 $(document).ready(function(){
-    $("#search-topics").click(function(){
+    $("#search1").keypress(function(){
       var xhttp = new XMLHttpRequest();
       xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
       xhttp.send();
@@ -26,17 +11,19 @@ $(document).ready(function(){
           if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(xhttp.responseText);
             var posts = response;
-            console.log(posts);
             var output = '';
-            for(var i = 0;i < 5;i++){ 
-              // u can use any value for 5 or photos.length
-              output += '<li  class="list-group-item d-flex justify-content-between align-items-center">'+posts[i].title+'</li>';
-              // output += '<li><h5>'+photos[i].title+'</h5></li><br />'+'<img src='
-              // +photos[i].url+
-              // ' alt="Smiley face" height="200" width="200">'+'<button type="button" class="btn btn-danger float-right">Delete</button><br /><br />';
-
-            }
-            document.getElementById('posts').innerHTML = output;
+            var inputVal = $(this).val();
+            console.log("onChange val",inputVal);
+            for(var i = 0;i <100;i++){ 
+                              // if(posts[i].title.includes(this.value)){ 
+                               console.log(posts[i]);
+                                output += '<li  class="list-group-item d-flex justify-content-between align-items-center">'+posts[i].title+'</li>';
+                              // } else{
+                              //   output += '';
+                              // }
+                          }
+                          console.log(output);
+                          document.getElementById('posts').innerHTML = output;
           } 
       };
 
@@ -44,47 +31,22 @@ $(document).ready(function(){
 });
 
 
-// qui est esse
-
-$(function(){
-  $("#search").keypress(function(){
-
-    // Dynamic Search Part Start
-      // var search = this.value;
-      // console.log("Search Value",search);
-
-
-
-      // var xhttp = new XMLHttpRequest();
-      // xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-      // xhttp.send();
-      // xhttp.onreadystatechange = function() {
-      //     if (this.readyState == 4 && this.status == 200) {
-      //       var response = JSON.parse(xhttp.responseText);
-
-            var posts = [{userId: 1, id: 1, title: "sunt aut facere", body: "quia et suscipit↵suscipit recusandae consequuntur"},
-             {userId: 1, id: 2, title: "qui est esse", body: "est rerum tempore vitae↵sequi sint nihil reprehend…aperiam non "},
-             {userId: 1, id: 3, title: "ea molestias quasi exercitationem ", body: "et iusto sed quo iure↵voluptatem occaecati omnis"},
-             {userId: 1, id: 4, title: "eum et est occaecati", body: "ullam et saepe reiciendis voluptatem adipisci↵sit "},
-             {userId: 1, id: 5, title: "nesciunt quas odio", body: "repudiandae veniam quaerat sunt sed↵alias aut "}];
-            console.log(posts);
-            var output = '';
-            for(var i = 0;i < 5;i++){ 
-                if(posts[i].title.includes(this.value)){
-                  output += '<li  class="list-group-item d-flex justify-content-between align-items-center">'+posts[i].title+'</li>';
-                } else{
-                  output += '';
-                }
-            }
-            console.log(output);
-            document.getElementById('posts').innerHTML = output;
-          // } 
-  //     };
-
+$(document).ready(function(){
+  $("#get").hide();
+  $("#post").hide();
+  $("#newsFeed").click(function(){
+    $("#get").hide();
+    $("#main").hide();
+    $("#post").show();
+  });
+  $("#search").click(function(){
+    $("#post").hide();
+    $("#main").hide();
+    $("#get").show();
+  });
+  $("#home").click(function(){
+    $("#get").hide();
+    $("#post").hide();
+    $("#main").show();
   });
 });
-
-// $('.chatinput').keyup(function(event) {
-//   newText = event.target.value;
-//   $('.printchatbox').text(newText);
-// });
